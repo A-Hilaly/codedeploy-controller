4d459e7cfffd980b050f328cde8c6e395f00d4ee
Amine Hilaly <hilalyamine@gmail.com>
2021-07-16 16:50:14 +0200

    publish-controller

---
+++ apis/v1alpha1/doc.go
@@ -0,0 +1,4 @@
+// +k8s:deepcopy-gen=package
+// Package v1alpha1 is the v1alpha1 version of the codedeploy.services.k8s.aws API.
+// +groupName=codedeploy.services.k8s.aws
+package v1alpha1
---
+++ apis/v1alpha2/doc.go
@@ -0,0 +1,4 @@
+// +k8s:deepcopy-gen=package
+// Package v1alpha2 is the v1alpha2 version of the codedeploy.services.k8s.aws API.
+// +groupName=codedeploy.services.k8s.aws
+package v1alpha2
